fae4feba109fae30e532604c961fdad46c853eb1
detollly <84504245+detollly@users.noreply.github.com>
2024-11-13 01:08:43 +0000

    changing background is set - need to adjust intervals

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const images = [
+    'url("/assets/img/Untitled-3.jpg")',
+    'url("/assets/img/Untitled-4.jpg")',
+    'url("/assets/img/Untitled-5.jpg")',
+    'url("/assets/img/Untitled-6.jpg")',
+    'url("/assets/img/Untitled-7.jpg")',
+    'url("/assets/img/Untitled-8.jpg")',
+    'url("/assets/img/Untitled-9.jpg")',
+    'url("/assets/img/Untitled-10.jpg")',
+    'url("/assets/img/Untitled-12.jpg")',
+    'url("/assets/img/Untitled-41.jpg")',
+    'url("/assets/img/Untitled-42.jpg")',
+    'url("/assets/img/Untitled-52.jpg")',
+    'url("/assets/img/Untitled-91.jpg")',
+    'url("/assets/img/Untitled-123.jpg")'
+];
+
+let currentIndex = 0; 
+
+function changeBackground() { 
+    const container = document.querySelector('.background-container'); 
+    container.style.backgroundImage = images[currentIndex]; 
+    currentIndex = (currentIndex + 1) % images.length; 
+} 
+    
+    
+    setInterval(changeBackground, 30000); 
+    window.onload = changeBackground;
